d51c25bfd9cfca9cfa0099ebc244fb9206e65db0
Julio Barrios <julio.barrios.9618@gmail.com>
2022-05-09 16:34:35 -0300

    View and Componet architect

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/App';
+import App from './routes/App';
 
 const rootElement = document.getElementById('app');
 const root = ReactDOM.createRoot(rootElement);
--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
+    publicPath: '/'
   },
   resolve: {
     extensions: ['.js', '.jsx'],
@@ -54,6 +55,7 @@ module.exports = {
     },
     compress: true,
     port: 3000,
-    open: true,
+    historyApiFallback: true,
+    open: true
   },
 };
