2b9bfcddf6a1404d322e895cca5f3fea66347621
Julio Barrios <julio.barrios.9618@gmail.com>
2022-05-10 22:18:45 -0300

    Integration with google maps

--- src/containers/Payment.jsx
+++ src/containers/Payment.jsx
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import AppContext from '../context/AppContext';
 import '../styles/components/Payment.css';
 
 function Payment() {
+  const { state: { cart } } = useContext(AppContext);
+
   return (
     <div className="Payment">
       <div className="Payment-content">
         <h3>Resumen del pedido</h3>
-        <div className="Payment-button">Boton de pago Paypal</div>
+        {cart.map((item) => (
+          <div className="Payment-item" key={item.title}>
+            <div className="Payment-element">
+              <h4>{item.title}</h4>
+              <span>
+                $
+                {' '}
+                {item.price}
+              </span>
+            </div>
+          </div>
+        ))}
+        <div className="Payment-button">
+          Boton de pago Paypal
+        </div>
       </div>
     </div>
   );
 }
 
-export default Payment;
+export default Payment; 
--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import AppContext from '../context/AppContext';
+import Map from '../components/Map'
 import '../styles/components/Success.css';
 
 function Success() {
+
+  const { state: { buyer } } = useContext(AppContext);
+
   return (
     <div className="Success">
       <div className="Success-content">
         <h2>Julio, Gracias por tu compra</h2>
         <span>Tu pedido llegara en 3 dias a tu dirección:</span>
-        <div className="Success-map">Google-maps</div>
+        <div className="Success-map">
+          <Map />
+        </div>
       </div>
     </div>
   );
