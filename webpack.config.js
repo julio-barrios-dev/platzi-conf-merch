84c20b8ea622c8d715a380af2da35710f3d438db
Julio Barrios <julio.barrios.9618@gmail.com>
2022-05-12 23:51:28 -0300

    [ADD] .firebaserc

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const Dotenv = require('dotenv-webpack');
+const { webpack } = require('webpack');
 
 module.exports = {
   entry: './src/index.js',
